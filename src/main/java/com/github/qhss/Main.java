765d59ab69cbeb1aa68ad54d46a7ae8f5a2be393
Mysterio1168 <95005225+Mysterio1168@users.noreply.github.com>
2023-05-17 12:19:02 +0000

    yes

--- src/main/java/com/github/qhss/Main.java
+++ src/main/java/com/github/qhss/Main.java
@@ -19,7 +19,7 @@ public class Main {
         api.addMessageCreateListener(
                 event -> {
                     if (event.getMessageContent().equalsIgnoreCase("!ping")) {
-                        event.getChannel().sendMessage(Card.J.getIcon());
+                        event.getChannel().sendMessage("Balls!");
                     }
                 });
     }
