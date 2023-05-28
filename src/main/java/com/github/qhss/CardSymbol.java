424aa21eeea8b880b628e8626f8d4d74a22446ea
FwankiL <89987389+FwankiL@users.noreply.github.com>
2023-05-28 03:42:24 +0000

    Added proper command parameters - changed image names to work with enum name - put redundant code into methods

--- src/main/java/com/github/qhss/CardSymbol.java
+++ src/main/java/com/github/qhss/CardSymbol.java
@@ -1,26 +1,24 @@
 package com.github.qhss;
 
 public enum CardSymbol {
-    A(11, "\uD83C\uDCC1"),
-    TWO(2, "🃂"),
-    THREE(3, "🃃"),
-    FOUR(4, "🃄"),
-    FIVE(5, "\uD83C\uDCC5"),
-    SIX(6, "\uD83C\uDCC6"),
-    SEVEN(7, "\uD83C\uDCC7"),
-    EIGHT(8, "\uD83C\uDCC8"),
-    NINE(9, "\uD83C\uDCC9"),
-    TEN(10, "\uD83C\uDCCA"),
-    J(10, "\uD83C\uDCCB"),
-    Q(10, "\uD83C\uDCCD"),
-    K(10, "\uD83C\uDCCE");
+    A(11),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    J(10),
+    Q(10),
+    K(10);
 
     private final int value;
-    private final String icon;
 
-    CardSymbol(int value, String icon) {
+    CardSymbol(int value) {
         this.value = value;
-        this.icon = icon;
     }
     public int getValue() {
         return value;
--- src/main/java/com/github/qhss/Main.java
+++ src/main/java/com/github/qhss/Main.java
@@ -7,42 +7,23 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.javacord.api.entity.intent.Intent;
+import org.javacord.api.entity.message.MessageBuilder;
+import org.javacord.api.entity.message.MessageDecoration;
 import org.javacord.api.entity.message.MessageFlag;
+import org.javacord.api.entity.message.component.ActionRow;
+import org.javacord.api.entity.message.component.Button;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.interaction.*;
 
 import java.awt.*;
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        // Dotenv dotenv = Dotenv.load();
-
-        // attempts to open a file
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        File f =
-                new File(
-                        classLoader
-                                .getResource("assets/PlayingCards/PNG-cards-1.3/c/2.png")
-                                .getFile());
-        System.out.println(f.length());
-
-        Gson gson = new Gson();
-        // ArrayList<Player> p = new ArrayList<>();
-        // p.add(new Player(100, "HELLO"));
-        // p.add(new Player(200, "JOHNNY"));
-
-        // // writes to a JSON file
-        // try (FileWriter fileWriter = new FileWriter("src/main/resources/balance.json")) {
-        //     gson.toJson(p, fileWriter);
-        //     fileWriter.close();
-        // } catch (IOException e) {
-        //     throw new RuntimeException(e);
-        // }
-
+        
         DiscordApi api =
                 new DiscordApiBuilder()
                         .setToken(System.getenv("DISCORD_TOKEN"))
@@ -50,23 +31,23 @@ public class Main {
                         .login()
                         .join();
 
-        SlashCommand command =
+        
+        /*
+         * think these only need to be run once but not sure
+         * will refactor later (maybe) 
+        */
+        SlashCommand games =
                 SlashCommand.with(
-                                "cb",
-                                "Card games",
-                                Arrays.asList(
-                                        SlashCommandOption.createWithChoices(
-                                                SlashCommandOptionType.STRING,
-                                                "game",
-                                                "Plays a game of something",
-                                                true,
-                                                Arrays.asList(
-                                                        SlashCommandOptionChoice.create(
-                                                                "blackjack", "bj"),
-                                                        SlashCommandOptionChoice.create(
-                                                                "_", "_")))))
+                        "cb",
+                        "Card games",
+                        Arrays.asList(
+                                SlashCommandOption.createDecimalOption("bj", "Plays a game of Blackjack", true, 1, 10000)))
                         .createGlobal(api)
                         .join();
+
+        SlashCommand daily = SlashCommand.with("cbdaily", "Claims daily money for Card Bot")
+                .createGlobal(api)
+                .join();
 
         Set<SlashCommand> commands = api.getGlobalSlashCommands().join();
 
@@ -74,10 +55,29 @@ public class Main {
                 event -> {
                     SlashCommandInteraction slashCommandInteraction =
                             event.getSlashCommandInteraction();
-                    String option =
-                            slashCommandInteraction.getArguments().get(0).getStringValue().get();
+                    Player[] array = read();
+                            
 
-                    if (option.equals("bj")) {
+                    
+                    if (slashCommandInteraction.getCommandName().equals("cbdaily")) {
+                        int plrIndex = findPlayer(array, slashCommandInteraction.getUser().getDiscriminatedName());
+                        if (plrIndex != -1) {
+                                array[plrIndex].setMoney(array[plrIndex].getMoney() + 500);
+                                write(array);
+                        }
+                        slashCommandInteraction.createImmediateResponder().setContent("Added 500. Your current balance: " + array[plrIndex].getMoney()).setFlags(MessageFlag.EPHEMERAL).respond();
+                        return;
+                    }
+                    SlashCommandInteractionOption s = slashCommandInteraction.getOptions().get(0);
+                    
+
+                    if (s.getName().equals("bj")) {
+                        File aFile = new File(
+                                classLoader
+                                        .getResource(
+                                                "assets/PlayingCards/PNG-cards-1.3/c/two.png")
+                                        .getFile());
+
                         EmbedBuilder embed =
                                 new EmbedBuilder()
                                         .setTitle("Blackjack")
@@ -86,109 +86,145 @@ public class Main {
                                         .addField("Your Cards: ", "VALUE")
                                         .setColor(Color.CYAN)
                                         .setFooter(
-                                                "Footer",
-                                                new File(
-                                                        classLoader
-                                                                .getResource(
-                                                                        "assets/PlayingCards/PNG-cards-1.3/c/2.png")
-                                                                .getFile()))
+                                                "Your bet: $" + s.getDecimalValue().get(),
+                                                aFile)
                                         .setThumbnail(
                                                 new File(
                                                         classLoader
                                                                 .getResource("assets/profile.png")
                                                                 .getFile()));
-
-                        slashCommandInteraction
-                                .createImmediateResponder()
-                                .addEmbed(embed)
-                                .respond();
-                    } else if (option.equals("_")) {
-                        slashCommandInteraction
-                                .createImmediateResponder()
-                                .setContent("Alternative working properly!")
-                                .setFlags(MessageFlag.EPHEMERAL)
-                                .respond();
+                        new MessageBuilder()
+                                .append("Pls work", MessageDecoration.BOLD)
+                                .addAttachment(aFile)
+                                .setEmbed(embed)
+                                .addComponents(
+                                        ActionRow.of(
+                                                Button.success("hit", "Hit", "👏", false),
+                                                Button.primary("stand", "Stand", "🧍", false),
+                                                Button.danger("double-down", "Double Down", "🤑", false),
+                                                Button.secondary("exit", "Exit", "🚪", false)
+                                        )
+                                )
+                                .send(slashCommandInteraction.getChannel().get());
                     }
                 });
         // Add a listener which answers with "Pong!" if someone writes "!ping"
         api.addMessageCreateListener(
                 event -> {
-                        String msg = event.getMessageContent();
-                        String author = event.getMessageAuthor().getDiscriminatedName();
-                        Player[] array;
+                        if (!event.getMessageAuthor().isBotUser()) {
+                                String msg = event.getMessageContent();
+                                String author = event.getMessageAuthor().getDiscriminatedName();
+                                Player[] array = read();
 
-                        try (Reader reader = new FileReader(classLoader.getResource("balance.json").getFile())) {
-                            array = gson.fromJson(reader, Player[].class);
-                            reader.close();
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
+                        if (msg.equalsIgnoreCase("!ping")) {
+                                Card c = new Card(CardSymbol.EIGHT, 'c');
+                                Card c4 = new Card(CardSymbol.FOUR, 'd');
 
-                    if (msg.equalsIgnoreCase("!ping")) {
-                        EmbedBuilder embed =
-                                new EmbedBuilder()
-                                        .setTitle("Blackjack")
-                                        .setDescription("Play against the dealer!")
-                                        .addField("Dealer's Cards: ", "VALUE")
-                                        .addField("Your Cards: ", "VALUE")
-                                        .setColor(Color.CYAN)
-                                        .setFooter(
-                                                "Footer",
-                                                new File(
-                                                        classLoader
-                                                                .getResource(
-                                                                        "assets/PlayingCards/PNG-cards-1.3/c/2.png")
-                                                                .getFile()))
-                                        .setThumbnail(
-                                                new File(
-                                                        classLoader
-                                                                .getResource("assets/profile.png")
-                                                                .getFile()));
+                                EmbedBuilder embed =
+                                        new EmbedBuilder()
+                                                .setTitle("Blackjack")
+                                                .setDescription("Play against the dealer!")
+                                                .addField("Dealer's Cards: ", "VALUE")
+                                                .addField("Your Cards: ", "VALUE")
+                                                .setColor(Color.CYAN)
+                                                .setImage(
+                                                        new File(
+                                                                classLoader
+                                                                        .getResource(
+                                                                                "assets/PlayingCards/PNG-cards-1.3/" + c4.suit() + "/" + c4.cardSymbol().name().toLowerCase() + ".png")
+                                                                        .getFile()))
+                                                .setFooter(
+                                                        "Footer",
+                                                        new File(
+                                                                classLoader
+                                                                        .getResource(
+                                                                                "assets/PlayingCards/PNG-cards-1.3/" + c.suit() + "/" + c.cardSymbol().name().toLowerCase() + ".png")
+                                                                        .getFile()))
+                                                .setThumbnail(
+                                                        new File(
+                                                                classLoader
+                                                                        .getResource("assets/profile.png")
+                                                                        .getFile()));
 
-                        event.getChannel().sendMessage(embed);
-                    } else if (msg.equalsIgnoreCase("!json")) {
-                        String message = "";
-                        for (Player player : array) {
-                            if (player.getUsername().equals(author)) {
-                                message = String.valueOf(player.getMoney());
-                            }
-                        }
-                        if (message.equals("")) {
-                            Player[] a = new Player[array.length + 1];
-                            for (int i = 0; i < array.length; i++) {
-                                a[i] = array[i];
-                            }
-                            a[array.length] = new Player(100, author);
-                
-                            try (FileWriter fileWriter = new FileWriter("src/main/resources/balance.json")) {
-                                gson.toJson(a, fileWriter);
-                                fileWriter.close();
-                            } catch (IOException e) {
-                                throw new RuntimeException(e);
-                            }
-                            message = author + ", money: " + a[array.length].getMoney();
-                        }
-
-                        event.getChannel().sendMessage(message);
-                    } else if (msg.equalsIgnoreCase("!money+")) {
-                        try (FileWriter fileWriter = new FileWriter("src/main/resources/balance.json")) {
-                            for (Player plr : array) {
-                                if (plr.getUsername().equals(author)) {
-                                    plr.setMoney(plr.getMoney() + 100);
+                                event.getChannel().sendMessage(embed);
+                        } else if (msg.equalsIgnoreCase("!json")) {
+                                String message = "";
+                                int plrIndex = findPlayer(array, author);
+                                if (plrIndex != -1) {
+                                        message = String.valueOf(array[plrIndex].getMoney());
                                 }
-                            }
-                            gson.toJson(array, fileWriter);
-                            fileWriter.close();
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
+                                else {
+                                Player[] a = new Player[array.length + 1];
+                                for (int i = 0; i < array.length; i++) {
+                                        a[i] = array[i];
+                                }
+                                a[array.length] = new Player(100, author);
+                        
+                                write(a);
+                                message = author + ", money: " + a[array.length].getMoney();
+                                }
+
+                                event.getChannel().sendMessage(message);
+                        } else if (msg.equalsIgnoreCase("!money+")) {
+                                int plrIndex = findPlayer(array, author);
+                                if (plrIndex != -1) {
+                                    array[plrIndex].setMoney(array[plrIndex].getMoney() + 100);
+                                }
+                                write(array);
+                                event.getChannel().sendMessage("money added! current money: " + array[array.length - 1].getMoney());
                         }
-                        event.getChannel().sendMessage("money added! current money: " + event.getMessageAuthor().getDisplayName());
-                    }
+                }
                 });
     }
 
-//     public static String appendPlayer(Player[] array, String username) {
+    /*
+     * finds a player in the array.
+     * returns -1 if not found
+     * 
+     * @param   players   the array of players to search in
+     * @param   username  the username of the Player
+     * 
+     * @return          The index of the player if found, else -1
+     */
+    public static int findPlayer(Player[] players, String username) {
+        for (int i = 0; i < players.length; i++) {
+                if (players[i].getUsername().equals(username))
+                        return i;
+        }
+        return -1;
+    }
 
-        
-//     }
+    /*
+     * finds a player in the array.
+     * returns -1 if not found
+     * 
+     * @param   players   the array of players to search in
+     * @param   username  the username of the Player
+     * 
+     * @return          The index of the player if found, else -1
+     */
+    public static void write(Player[] array) {
+        try (FileWriter fileWriter = new FileWriter("src/main/resources/balance.json")) {
+                new Gson().toJson(array, fileWriter);
+        } catch (IOException e) {
+                throw new RuntimeException(e);
+        }
+    }
+
+    /*
+     * finds a player in the array.
+     * returns -1 if not found
+     * 
+     * @param   players   the array of players to search in
+     * @param   username  the username of the Player
+     * 
+     * @return          The index of the player if found, else -1
+     */
+    public static Player[] read() {
+        try (Reader reader = new FileReader("src/main/resources/balance.json")) {
+                return new Gson().fromJson(reader, Player[].class);
+        } catch (IOException e) {
+                throw new RuntimeException(e);
+        }
+    }
 }
